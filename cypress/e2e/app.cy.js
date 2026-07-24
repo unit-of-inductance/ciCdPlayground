170b6899eeecfe733842da475c3629e18ec49c31
Henry Ruben Fischer <henryruben.fischer@tngtech.com>
2026-07-24 15:01:22 +0200

    Fix butler test

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,9 +1,9 @@
 // app.spec.js created with Cypress
 //
 describe('The app', () => {
-    it('tells that you are a butler', () => {
+    it('tells that you are a fisher', () => {
         cy.visit('')
         cy.get('h1')
-        .should('have.text', 'Hello, my name is James Butler!')
+        .should('have.text', 'Hello, my name is Henry Ruben Fischer!')
     })
   })
